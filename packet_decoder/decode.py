c6037c589bb83505f38c282e42f82282c93acee4
Nikolaus J. Reichert <nikolaus@njreichert.ca>
2023-05-25 21:54:04 -0500

    Make many changes to get an AX.25 packet decoding

--- packet_decoder/decode.py
+++ packet_decoder/decode.py
@@ -22,6 +22,8 @@ ABORT_SEQ = "0b111111111111111"
 POST_BITSTUFF_SEQ = "0b111110"
 PRE_BITSTUFF_SEQ = "0b11111"
 
+MIN_PACKET_SIZE = 17
+
 data_queue = Queue()
 
 def log(string: str) -> None:
@@ -29,22 +31,11 @@ def log(string: str) -> None:
     print(f"{curr_time}: {string}")
 
 """
-Reverse the order of bits in each byte of a bytearray, preserving endianness.
-"""
-def reverse_bits(b: bytes) -> bytes:
-    output = b""
-
-    for byte in b:
-        output.append((BitArray(byte)[::-1]).tobytes())
-
-    return output
-
-"""
 Shift all bytes in the given array by n bits right.
 """
-def shift_bytes(b: bytes, n: int) -> bytes:
+def shift_bytes(b: bytearray, n: int) -> bytearray:
     assert(n >= 0 and n <= 8)
-    output = b""
+    output = bytearray()
 
     for byte in b:
         output.append(byte >> n)
@@ -54,31 +45,40 @@ def shift_bytes(b: bytes, n: int) -> bytes:
 def parse_packet(packet_bits: BitArray) -> None:
     # Everything but the flag fields are bitstuffed.
     # TODO: TEST THIS!!!!!!!!!!!!!! -NJR
-    decoded_data = packet_bits[8:-8].replace(POST_BITSTUFF_SEQ,
-                                             PRE_BITSTUFF_SEQ)
+
+    # Get rid of the flag.
+    flag_stripped = packet_bits[8:-8]
+
+    flag_stripped.replace(POST_BITSTUFF_SEQ, PRE_BITSTUFF_SEQ)
     
-    # TODO: Reverse bit order?
-    decoded_bytes = packet_bits[8:-24].tobytes()
+    # Reverse the order of the bits to MSB First (I think??????) -NJR
+    msb_first = flag_stripped
+    for i in range(len(msb_first)):
+        msb_first[8*i:8*i + 8] = msb_first[8*i:8*i + 8][::-1]
+
+    decoded_bytes = bytearray(msb_first[:-16].tobytes())
+
     
     # CRC Bits: All ones to start, most significant bit first, Big-endian. (ISO-3309)
     # TODO: Actually do the CRC Check. There is a lot of Uncertainty with it online 
     # so figure it out later.
-    if (len(decoded_data) < 136 
-        or len(decoded_data) % 8 != 0):
+    if (len(decoded_bytes) < MIN_PACKET_SIZE):
         log("Invalid packet decoded!")
-    else:
-        to_call = reverse_bits(shift_bytes(decoded_bytes[0:6]))
-        to_ssid = (decoded_bytes[6:7] >> 1) & 0x0F
 
-        from_call = reverse_bits(shift_bytes(decoded_bytes[7:13]))
-        from_ssid = (decoded_bytes[13:14] >> 1) & 0x0F
+    else:
+        to_call = str(shift_bytes(decoded_bytes[0:6], 1), "utf-8")
+        to_ssid = str((int(decoded_bytes[6]) >> 1) & 0x0F)
+
+        from_call = str(shift_bytes(decoded_bytes[7:13], 1), "utf-8")
+        from_ssid = str((int(decoded_bytes[13]) >> 1) & 0x0F)
 
         control_bits = decoded_bytes[14]
         protocol_id = decoded_bytes[15]
 
-        info_field = reverse_bits(decoded_bytes[16:])
+        info_field = str(decoded_bytes[16:], "utf-8")
 
-        log(f"Decoded Frame: {to_call}-{to_ssid}>{from_call}-{from_ssid}: {info_field}")
+        log(f"Decoded Frame: {from_call}-{from_ssid}>{to_call}-{to_ssid}: {info_field}")
+        pass
 
 def consume_data() -> None:
     packet_bits = BitArray()
@@ -86,20 +86,32 @@ def consume_data() -> None:
 
     while (True):
         # Simply block until we get the data.
-        new_data = data_queue.get()
+        new_data = BitArray(data_queue.get())
+
+        # for i in range(len(new_data)):
+        #     new_data[8*i:8*i + 8] = new_data[8*i:8*i + 8][::-1]
+
+
         packet_bits.append(new_data)
 
         # Look for a start and end. 
-        if (start_pos := packet_bits.find(FLAG_SEQ)):
+        if ((start_pos := packet_bits.find(FLAG_SEQ))):
+            # and not packet_bits.find(ABORT_SEQ, start=start_pos[0])):
+
+            # We need to flip the order of the bits 
+
+            print("Start found!")
             # BitArray.find() produces a tuple (to allow for boolean )
-            if (end_pos := packet_bits.find(FLAG_SEQ, start=(start_pos[0]+8)) 
-                and not packet_bits.find(ABORT_SEQ, start=(start_pos[0]+8))):
+            if (end_pos := packet_bits.find(FLAG_SEQ, start=(start_pos[0]+8))):
+                
+                print("End Found!")
+
                 packet_data = packet_bits[start_pos[0]:end_pos[0]+8]
                 packet_bits = packet_bits[end_pos[0]+8:]
 
                 parse_packet(packet_data)
                 
-                print(f"{curr_time} Packet found: {str(packet_data.tobytes())}")
+                log(f"Packet found: {str(packet_data.tobytes())}")
         else:
             # The flag might not be fully here yet,
             # so get rid of everything but the last <8 bits.
