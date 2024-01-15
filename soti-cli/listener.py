eb7a2c83fcba4113658e7ade41d9b562e264a6bd
edvirs <andrii_kk@protonmail.com>
2024-01-15 17:10:39 -0600

    redis server usage added

--- soti-cli/listener.py
+++ soti-cli/listener.py
@@ -7,24 +7,40 @@ from cli_utils.constants import (
 
 from cli_utils.command_args import parsers, parse_generic
 
-from shared_queue import msg_queue
+#from shared_queue import msg_queue
 
-import json, serial, sys, datetime, queue, time
+import json, sys, datetime, queue, time, redis
+import serial
+
+r = redis.Redis(host="localhost", port=6379, db=0)
+r.mset({"byte0":"", "byte1":"", "byte2":"", "byte3":"", "byte4":"", "byte5":"", "byte6":"", "byte7":"", 
+        "byte8":"", "byte9":"", "byte10":"", "byte11":""})
+
+msg_queue = queue.Queue()
 
 # first script argument will be the device to read/write to
 #port_arg = sys.argv[1]
-port_arg = "/dev/tty.Bluetooth-Incoming-Port"  
+port_arg = "/dev/tty.Bluetooth-Incoming-Port"
+new_msg = [] 
+data = []  
+
+ser = serial.Serial(port_arg, baudrate=115200)
 
 def listen():
     print("Running")
-    with serial.Serial(port_arg, baudrate=115200) as ser:
-        while True:
-            # block and read indefinitely, reading messages 11 bytes at a time
-             #new_msg = ser.read(MSG_SIZE)
-             new_msg = bytes(11)
-             msg_queue.put(new_msg)
-             print(f"New Message: {new_msg.hex()}")
-             time.sleep(1)
+    while True:
+        # block and read indefinitely, reading messages 11 bytes at a time
+        
+        new_msg = bytes(11)
+        
+        for i in range(11):
+            r.set("byte{number}".format(number=str(i)), new_msg[i])
+        
+        data.append(new_msg)
+
+        msg_queue.put(new_msg)
+        print(f"New Message: {new_msg.hex()}")
+        time.sleep(1)
 
 if __name__ == "__main__":
     listen()
