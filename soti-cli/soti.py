9f8f7c82d718b0c71b1939103770e2583268e692
Daigh Burgess <daighb@outlook.com>
2024-01-31 18:48:46 -0600

    Create soti.py

---
+++ soti-cli/soti.py
@@ -0,0 +1,18 @@
+'''
+ * FILENAME: soti.py
+ *
+ * DESCRIPTION: The main soti front-end script which initializes the terminal and listener threads.
+ * 
+ * NOTES: Massive credit to Mia for implementing the SOTI front-end.
+ *        The following code is heavily inspired from her initial C Shell program.
+ *
+ * AUTHORS:
+ *  - Mia
+ *  - Christina
+ *  - Daigh
+ *  - Arnav
+ *
+ * CREATED ON: Jan 31, 2024
+'''
+
+import multiprocessing
