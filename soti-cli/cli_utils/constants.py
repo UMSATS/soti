cb40248cc5789919fbc2b634ae89514555907cb2
ArnavG-it <arnavgupta468@outlook.com>
2024-09-16 15:51:32 -0500

    style: fix indentation

--- soti-cli/cli_utils/constants.py
+++ soti-cli/cli_utils/constants.py
@@ -17,182 +17,182 @@ class NodeID(Enum):
 NUM_COMMANDS = 40
 class CmdID(Enum): (
     ############################################
-	### COMMON
-	############################################
-	CMD_COMM_RESET,
-	CMD_COMM_PREPARE_FOR_SHUTDOWN,
-	CMD_COMM_GET_TELEMETRY,
-	CMD_COMM_SET_TELEMETRY_INTERVAL,
-	CMD_COMM_GET_TELEMETRY_INTERVAL,
-	CMD_COMM_UPDATE_START,
-	CMD_COMM_UPDATE_LOAD,
-	CMD_COMM_UPDATE_END,
+    ### COMMON
+    ############################################
+    CMD_COMM_RESET,
+    CMD_COMM_PREPARE_FOR_SHUTDOWN,
+    CMD_COMM_GET_TELEMETRY,
+    CMD_COMM_SET_TELEMETRY_INTERVAL,
+    CMD_COMM_GET_TELEMETRY_INTERVAL,
+    CMD_COMM_UPDATE_START,
+    CMD_COMM_UPDATE_LOAD,
+    CMD_COMM_UPDATE_END,
 
-	############################################
-	### CDH
-	############################################
-	## Event Processing.
-	CMD_CDH_PROCESS_HEARTBEAT,
-	CMD_CDH_PROCESS_RUNTIME_ERROR,
-	CMD_CDH_PROCESS_COMMAND_ERROR,
-	CMD_CDH_PROCESS_NOTIFICATION,
-	CMD_CDH_PROCESS_TELEMETRY_REPORT,
-	CMD_CDH_PROCESS_RETURN,
-	CMD_CDH_PROCESS_LED_TEST,
+    ############################################
+    ### CDH
+    ############################################
+    ## Event Processing.
+    CMD_CDH_PROCESS_HEARTBEAT,
+    CMD_CDH_PROCESS_RUNTIME_ERROR,
+    CMD_CDH_PROCESS_COMMAND_ERROR,
+    CMD_CDH_PROCESS_NOTIFICATION,
+    CMD_CDH_PROCESS_TELEMETRY_REPORT,
+    CMD_CDH_PROCESS_RETURN,
+    CMD_CDH_PROCESS_LED_TEST,   
 
-	## Clock
-	CMD_CDH_SET_RTC,
-	CMD_CDH_GET_RTC,
+    ## Clock
+    CMD_CDH_SET_RTC,
+    CMD_CDH_GET_RTC,
 
-	## Tests
-	CMD_CDH_TEST_FLASH,
-	CMD_CDH_TEST_MRAM,
+    ## Tests
+    CMD_CDH_TEST_FLASH,
+    CMD_CDH_TEST_MRAM,
 
-	CMD_CDH_RESET_SUBSYSTEM,
+    CMD_CDH_RESET_SUBSYSTEM,
 
-	## Antenna
-	CMD_CDH_ENABLE_ANTENNA,
-	CMD_CDH_DEPLOY_ANTENNA,
+    ## Antenna
+    CMD_CDH_ENABLE_ANTENNA,
+    CMD_CDH_DEPLOY_ANTENNA,
 
-	############################################
-	### POWER
-	############################################
-	CMD_PWR_PROCESS_HEARTBEAT,
-	CMD_PWR_SET_SUBSYSTEM_POWER,
-	CMD_PWR_GET_SUBSYSTEM_POWER,
-	CMD_PWR_SET_BATTERY_HEATER_POWER,
-	CMD_PWR_GET_BATTERY_HEATER_POWER,
-	CMD_PWR_SET_BATTERY_ACCESS,
-	CMD_PWR_GET_BATTERY_ACCESS,
+    ############################################
+    ### POWER
+    ############################################
+    CMD_PWR_PROCESS_HEARTBEAT,
+    CMD_PWR_SET_SUBSYSTEM_POWER,
+    CMD_PWR_GET_SUBSYSTEM_POWER,
+    CMD_PWR_SET_BATTERY_HEATER_POWER,
+    CMD_PWR_GET_BATTERY_HEATER_POWER,
+    CMD_PWR_SET_BATTERY_ACCESS,
+    CMD_PWR_GET_BATTERY_ACCESS,
 
-	############################################
-	### ADCS
-	############################################
-	CMD_ADCS_SET_MAGNETORQUER_DIRECTION,
-	CMD_ADCS_GET_MAGNETORQUER_DIRECTION,
-	CMD_ADCS_SET_OPERATING_MODE,
-	CMD_ADCS_GET_OPERATING_MODE,
+    ############################################
+    ### ADCS
+    ############################################
+    CMD_ADCS_SET_MAGNETORQUER_DIRECTION,
+    CMD_ADCS_GET_MAGNETORQUER_DIRECTION,
+    CMD_ADCS_SET_OPERATING_MODE,
+    CMD_ADCS_GET_OPERATING_MODE,
 
-	############################################
-	### PAYLOAD
-	############################################
-	CMD_PLD_SET_ACTIVE_ENVS,
-	CMD_PLD_GET_ACTIVE_ENVS,
-	CMD_PLD_SET_SETPOINT,
-	CMD_PLD_GET_SETPOINT,
-	CMD_PLD_SET_TOLERANCE,
-	CMD_PLD_GET_TOLERANCE,
-	CMD_PLD_TEST_LEDS
+    ############################################
+    ### PAYLOAD
+    ############################################
+    CMD_PLD_SET_ACTIVE_ENVS,
+    CMD_PLD_GET_ACTIVE_ENVS,
+    CMD_PLD_SET_SETPOINT,
+    CMD_PLD_GET_SETPOINT,
+    CMD_PLD_SET_TOLERANCE,
+    CMD_PLD_GET_TOLERANCE,
+    CMD_PLD_TEST_LEDS
 ) = range(NUM_COMMANDS)
 
 # constants for command codes & priorities
 COMM_INFO = {
     ############################################
-	### COMMON
-	############################################
-	CmdID.CMD_COMM_RESET.value:
+    ### COMMON
+    ############################################
+    CmdID.CMD_COMM_RESET.value:
     {"priority": 0, "dest": None},
-	CmdID.CMD_COMM_PREPARE_FOR_SHUTDOWN.value:
+    CmdID.CMD_COMM_PREPARE_FOR_SHUTDOWN.value:
     {"priority": 1, "dest": None},
-	CmdID.CMD_COMM_GET_TELEMETRY.value:
+    CmdID.CMD_COMM_GET_TELEMETRY.value:
     {"priority": 32, "dest": None},
-	CmdID.CMD_COMM_SET_TELEMETRY_INTERVAL.value:
+    CmdID.CMD_COMM_SET_TELEMETRY_INTERVAL.value:
     {"priority": 32, "dest": None},
-	CmdID.CMD_COMM_GET_TELEMETRY_INTERVAL.value: 
+    CmdID.CMD_COMM_GET_TELEMETRY_INTERVAL.value: 
     {"priority": 32, "dest": None},
-	CmdID.CMD_COMM_UPDATE_START.value: 
+    CmdID.CMD_COMM_UPDATE_START.value: 
     {"priority": 32, "dest": None},
-	CmdID.CMD_COMM_UPDATE_LOAD.value: 
+    CmdID.CMD_COMM_UPDATE_LOAD.value: 
     {"priority": 32, "dest": None},
-	CmdID.CMD_COMM_UPDATE_END.value: 
+    CmdID.CMD_COMM_UPDATE_END.value: 
     {"priority": 32, "dest": None},
 
-	############################################
-	### CDH
-	############################################
-	## Event Processing.
-	CmdID.CMD_CDH_PROCESS_HEARTBEAT.value:
+    ############################################
+    ### CDH
+    ############################################
+    ## Event Processing.
+    CmdID.CMD_CDH_PROCESS_HEARTBEAT.value:
     {"priority": 2, "dest": None},
-	CmdID.CMD_CDH_PROCESS_RUNTIME_ERROR.value: 
+    CmdID.CMD_CDH_PROCESS_RUNTIME_ERROR.value: 
     {"priority": 10, "dest": None},
-	CmdID.CMD_CDH_PROCESS_COMMAND_ERROR.value: 
+    CmdID.CMD_CDH_PROCESS_COMMAND_ERROR.value: 
     {"priority": 10, "dest": None},
-	CmdID.CMD_CDH_PROCESS_NOTIFICATION.value: 
+    CmdID.CMD_CDH_PROCESS_NOTIFICATION.value: 
     {"priority": 2, "dest": None},
-	CmdID.CMD_CDH_PROCESS_TELEMETRY_REPORT.value: 
+    CmdID.CMD_CDH_PROCESS_TELEMETRY_REPORT.value: 
     {"priority": 3, "dest": None},
-	CmdID.CMD_CDH_PROCESS_RETURN.value: 
+    CmdID.CMD_CDH_PROCESS_RETURN.value: 
     {"priority": 32, "dest": None},
-	CmdID.CMD_CDH_PROCESS_LED_TEST.value: 
+    CmdID.CMD_CDH_PROCESS_LED_TEST.value: 
     {"priority": 4, "dest": None},
 
-	## Clock
-	CmdID.CMD_CDH_SET_RTC.value: 
+    ## Clock
+    CmdID.CMD_CDH_SET_RTC.value: 
     {"priority": 32, "dest": None},
-	CmdID.CMD_CDH_GET_RTC.value: 
-    {"priority": 32, "dest": None},
-
-	## Tests
-	CmdID.CMD_CDH_TEST_FLASH.value: 
-    {"priority": 32, "dest": None},
-	CmdID.CMD_CDH_TEST_MRAM.value: 
+    CmdID.CMD_CDH_GET_RTC.value: 
     {"priority": 32, "dest": None},
 
-	CmdID.CMD_CDH_RESET_SUBSYSTEM.value: 
+    ## Tests
+    CmdID.CMD_CDH_TEST_FLASH.value: 
+    {"priority": 32, "dest": None},
+    CmdID.CMD_CDH_TEST_MRAM.value: 
     {"priority": 32, "dest": None},
 
-	## Antenna
-	CmdID.CMD_CDH_ENABLE_ANTENNA.value: 
-    {"priority": 32, "dest": None},
-	CmdID.CMD_CDH_DEPLOY_ANTENNA.value: 
+    CmdID.CMD_CDH_RESET_SUBSYSTEM.value: 
     {"priority": 32, "dest": None},
 
-	############################################
-	### POWER
-	############################################
-	CmdID.CMD_PWR_PROCESS_HEARTBEAT.value: 
+    ## Antenna
+    CmdID.CMD_CDH_ENABLE_ANTENNA.value: 
+    {"priority": 32, "dest": None},
+    CmdID.CMD_CDH_DEPLOY_ANTENNA.value: 
+    {"priority": 32, "dest": None},
+
+    ############################################
+    ### POWER
+    ############################################
+    CmdID.CMD_PWR_PROCESS_HEARTBEAT.value: 
     {"priority": 2, "dest": None},
-	CmdID.CMD_PWR_SET_SUBSYSTEM_POWER.value: 
+    CmdID.CMD_PWR_SET_SUBSYSTEM_POWER.value: 
     {"priority": 0, "dest": None},
-	CmdID.CMD_PWR_GET_SUBSYSTEM_POWER.value: 
+    CmdID.CMD_PWR_GET_SUBSYSTEM_POWER.value: 
     {"priority": 32, "dest": None},
-	CmdID.CMD_PWR_SET_BATTERY_HEATER_POWER.value: 
+    CmdID.CMD_PWR_SET_BATTERY_HEATER_POWER.value: 
     {"priority": 5, "dest": None},
-	CmdID.CMD_PWR_GET_BATTERY_HEATER_POWER.value: 
+    CmdID.CMD_PWR_GET_BATTERY_HEATER_POWER.value: 
     {"priority": 32, "dest": None},
-	CmdID.CMD_PWR_SET_BATTERY_ACCESS.value: 
+    CmdID.CMD_PWR_SET_BATTERY_ACCESS.value: 
     {"priority": 32, "dest": None},
-	CmdID.CMD_PWR_GET_BATTERY_ACCESS.value: 
-    {"priority": 32, "dest": None},
-
-	############################################
-	### ADCS
-	############################################
-	CmdID.CMD_ADCS_SET_MAGNETORQUER_DIRECTION.value: 
-    {"priority": 32, "dest": None},
-	CmdID.CMD_ADCS_GET_MAGNETORQUER_DIRECTION.value: 
-    {"priority": 32, "dest": None},
-	CmdID.CMD_ADCS_SET_OPERATING_MODE.value: 
-    {"priority": 32, "dest": None},
-	CmdID.CMD_ADCS_GET_OPERATING_MODE.value: 
+    CmdID.CMD_PWR_GET_BATTERY_ACCESS.value: 
     {"priority": 32, "dest": None},
 
-	############################################
-	### PAYLOAD
-	############################################
-	CmdID.CMD_PLD_SET_ACTIVE_ENVS.value: 
+    ############################################
+    ### ADCS
+    ############################################
+    CmdID.CMD_ADCS_SET_MAGNETORQUER_DIRECTION.value: 
     {"priority": 32, "dest": None},
-	CmdID.CMD_PLD_GET_ACTIVE_ENVS.value: 
+    CmdID.CMD_ADCS_GET_MAGNETORQUER_DIRECTION.value: 
     {"priority": 32, "dest": None},
-	CmdID.CMD_PLD_SET_SETPOINT.value: 
+    CmdID.CMD_ADCS_SET_OPERATING_MODE.value: 
     {"priority": 32, "dest": None},
-	CmdID.CMD_PLD_GET_SETPOINT.value: 
+    CmdID.CMD_ADCS_GET_OPERATING_MODE.value: 
     {"priority": 32, "dest": None},
-	CmdID.CMD_PLD_SET_TOLERANCE.value: 
+
+    ############################################
+    ### PAYLOAD
+    ############################################
+    CmdID.CMD_PLD_SET_ACTIVE_ENVS.value: 
     {"priority": 32, "dest": None},
-	CmdID.CMD_PLD_GET_TOLERANCE.value: 
+    CmdID.CMD_PLD_GET_ACTIVE_ENVS.value: 
     {"priority": 32, "dest": None},
-	CmdID.CMD_PLD_TEST_LEDS.value: 
+    CmdID.CMD_PLD_SET_SETPOINT.value: 
+    {"priority": 32, "dest": None},
+    CmdID.CMD_PLD_GET_SETPOINT.value: 
+    {"priority": 32, "dest": None},
+    CmdID.CMD_PLD_SET_TOLERANCE.value: 
+    {"priority": 32, "dest": None},
+    CmdID.CMD_PLD_GET_TOLERANCE.value: 
+    {"priority": 32, "dest": None},
+    CmdID.CMD_PLD_TEST_LEDS.value: 
     {"priority": 4, "dest": None}
 }
 # Add command names using CmdID
